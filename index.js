705c1c23d586b5c002efd6cf87df97cb1f939a53
UWASE Clementine <clemawase@gmail.com>
2023-11-21 05:00:45 +0200

    files added

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let num= 2;
+num=1;
+console.log(num);
+let a;
+a = 9;
+console.log(a);
+let numOnene = 5;
+let numTwo = numOne;
+console.log(numTwo);
+let numOne = 5;
+let numTwo = numOne;
+console.log(numTwo);
