5e40577f0d6a97d660bf20bda90c90d188ce6446
UWASE Clementine <clemawase@gmail.com>
2023-11-22 04:29:11 +0200

    boolean

---
+++ day1/index.js
@@ -0,0 +1,10 @@
+
+let num = 2;
+num = 1;
+console.log(num);
+let a;
+a = 9;
+console.log(a);
+// declaration day2
+const sun = 5;
+console.log(sun);
---
+++ day2/day2.js
@@ -0,0 +1,9 @@
+// boolean
+let m = false;
+console.log(m);
+let g = true;
+console.log(g);
+let isTrue;
+isTrue = true;
+console.log(isTrue);
+
