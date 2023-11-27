cb5747defd4e2a96541bcbf2da17adf2fe879643
UWASE Clementine <clemawase@gmail.com>
2023-11-27 04:36:05 +0200

    exercises in day4

---
+++ day4/da4.js
@@ -0,0 +1,56 @@
+// block program
+let counter;
+console.log(counter);
+{
+    counter = 2;
+    console.log(counter);
+}
+counter = counter + 5;
+console.log(counter);
+let height = 180;
+let weight = 90;
+{
+
+    console.log(height);
+    console.log(weight);
+}
+height =  height + 5;
+weight =  weight + 7;
+console.log(height);
+console.log(weight);
+height = height + weight;
+let candle = 98;
+{
+    let bundle = 89;
+    let henz = "welcome";
+    {
+        
+        console.log(candle);
+        console.log(bundle);
+        console.log(henz);
+    }
+    console.log(candle);
+    console.log(bundle);
+    console.log(henz);
+    let cally =3;
+    
+    {
+       
+        let san =9;
+        console.log(cally);
+        console.log(san);
+    }
+    
+}
+var holl = 4;
+{
+    var goal = 5;
+    console.log(holl);
+    console.log(goal);
+}
+
+
+
+
+
+
